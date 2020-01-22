fad9df618bcd57cebdad12c1c8a277696c4ac23b
Nasim <nasim.sonboli@gmail.com>
2020-01-21 23:01:36 -0700

    ItemCoverageEvaluator debugged and corrected.

--- core/src/main/java/net/librec/eval/ranking/ItemCoverageEvaluator.java
+++ core/src/main/java/net/librec/eval/ranking/ItemCoverageEvaluator.java
@@ -24,11 +24,12 @@ import net.librec.recommender.item.UserItemRatingEntry;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Finds the ratio of unique items recommended of users to total unique items in testMatrix
  *  *
- * @author Florencia Cabral
+ * @author Nasim Sonboli, Florencia Cabral
  */
 
 public class ItemCoverageEvaluator extends AbstractRecommenderEvaluator {
@@ -41,27 +42,33 @@ public class ItemCoverageEvaluator extends AbstractRecommenderEvaluator {
 
         // set up iterator for recommended items list
         Iterator<UserItemRatingEntry> recommendedEntryIter = recommendedList.entryIterator();
+        recommendedList.userIterator();
 
         // initialize list for unique items in recommended items list
         List<Integer> uniqueItemsRecList = new java.util.ArrayList<Integer>();
+        List<Integer> uniqueItemsInTestMatrix = new java.util.ArrayList<Integer>();
 
-        // get list of unique items in testMatrix, by unique index number
-        int[] uniqueItemsInTestMatrix = testMatrix.getColumnIndices();
+        int numUsers = testMatrix.numRows();
+        // create a unique list of items from the test matrix
+        for (int userID = 0; userID < numUsers; userID++) {
+            Set<Integer> testSetByUser = testMatrix.getColumnsSet(userID);
+            for (int itemId: testSetByUser) {
+                if (uniqueItemsInTestMatrix.size() == 0 || !uniqueItemsInTestMatrix.contains(itemId)) {
+                    uniqueItemsInTestMatrix.add(itemId);
+                }
+            }
+        }
 
-        int index = 0;
 
         // iterate through recommended items list, add unique items in testMatrix found
         while (recommendedEntryIter.hasNext()) {
             UserItemRatingEntry userItemRatingEntry = recommendedEntryIter.next();
-
             if (uniqueItemsRecList.size() == 0 || !uniqueItemsRecList.contains(userItemRatingEntry.getItemIdx())) {
-                uniqueItemsRecList.add(index, userItemRatingEntry.getItemIdx());
-                index++;
+                uniqueItemsRecList.add(userItemRatingEntry.getItemIdx());
             }
         }
 
-    // return ratio of unique items in recommended list to number of unique items in testMatrix
-        return uniqueItemsInTestMatrix.length > 0 ? uniqueItemsRecList.size() * 1.0 / uniqueItemsInTestMatrix.length : 0.0d;
+        // return ratio of unique items in recommended list to number of unique items in testMatrix
+        return uniqueItemsInTestMatrix.size() > 0 ? uniqueItemsRecList.size() * 1.0 / uniqueItemsInTestMatrix.size() : 0.0d;
     }
-
 }
